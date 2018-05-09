6c1e68d76e890be89e17dd511dce66729728c609
Alvin Voo <alvin.voo@gmail.com>
2018-05-09 15:19:10 +0800

    added for mongoose library

---
+++ server/models/Todo.js
@@ -0,0 +1,18 @@
+let todoSchema = mongoose.Schema({
+  text: {
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  },
+  completed: {
+    type:Boolean,
+    default:false
+  },
+  completedAt: {
+    type:Number,
+    default:null
+  }
+});
+
+module.exports = mongoose.model('Todo',todoSchema);
---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+let userSchema = mongoose.Schema({
+  email:{
+    type:String,
+    trim:true,
+    minlength:1,
+    required:true
+  }
+})
+
+module.exports = mongoose.model('User',userSchema);
